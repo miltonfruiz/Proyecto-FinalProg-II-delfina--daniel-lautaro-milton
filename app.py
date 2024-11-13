ac52eb2622ae58c611a1e465e2abe06cae68ba23
Milton Franco Ruiz <miltonfruizok@outlook.es>
2024-11-13 15:27:58 -0300

    Creación de Base de Datos (subscribers.db) + configuraciones

--- app.py
+++ app.py
@@ -1,25 +1,55 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
 
+def init_db():
+    conn = sqlite3.connect('subscribers.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS subscribers (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT,
+                    email TEXT,
+                    phone TEXT,  -- Cambié # a -- aquí
+                    payment_option TEXT,
+                    payment_method TEXT,
+                    card_type TEXT)''')
+    c.execute("PRAGMA table_info(subscribers);")
+    columns = [column[1] for column in c.fetchall()]
+    if 'phone' not in columns:
+        c.execute('ALTER TABLE subscribers ADD COLUMN phone TEXT')
+    conn.commit()
+    conn.close()
+
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/subscribe', methods=['POST'])
 def subscribe():
-    data = {
-        "name": request.form.get("name"),
-        "email": request.form.get("email"),
-        "phone": request.form.get("phone"),
-        "paymentOption": request.form.get("paymentOption"),
-        "paymentMethod": request.form.get("paymentMethod"),
-    }
-    return jsonify({"success": True, "message": "Subscription successful!"})
+    data = request.get_json()
+    name = data['name']
+    email = data['email']
+    phone = data['phone']
+    payment_option = data['paymentOption']
+    payment_method = data['paymentMethod']
+    card_type = data.get('cardType', '')
+    conn = sqlite3.connect('subscribers.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO subscribers (name, email, phone, payment_option, payment_method, card_type) VALUES (?, ?, ?, ?, ?, ?)", 
+              (name, email, phone, payment_option, payment_method, card_type))
+    conn.commit()
+    conn.close()
+    return jsonify(success=True)
 
 @app.route('/internacional')
 def internacional():
-    return render_template('delfi-internacional.html')
+    return render_template('internacional.html')
+
+@app.route('/redirect_to_index')
+def redirect_to_index():
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
